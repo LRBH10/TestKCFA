c479c03b45887f6920a711d4a5d0105c3a277785
rlaouadi <rabah.laouadi@pradeo.net>
2015-02-12 09:55:53 +0100

    Source ADDer

---
+++ TestKLCFA/src/net/pradeo/KCFATest/MainSourceAdder.java
@@ -0,0 +1,14 @@
+package net.pradeo.KCFATest;
+
+import net.pradeo.sourceAdder.SourceAdder;
+
+public class MainSourceAdder {
+  public static void main(String[] args) {
+    SourceAdder src = new SourceAdder();
+
+    src.initAnimals();
+    src.initAnimals();
+
+    src.printAnimals();
+  }
+}
